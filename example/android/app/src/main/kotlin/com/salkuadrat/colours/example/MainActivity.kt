0cb0f6b39269f767949c79c04992c54f9a90ae09
Salman S <salkuadrat@gmail.com>
2020-09-24 12:11:01 +0700

    example

---
+++ example/android/app/src/main/kotlin/com/salkuadrat/colours/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.salkuadrat.colours.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:colours/colours.dart';
+import 'package:flutter/material.dart';
+
+MaterialColor gojekSwatch = Colours.swatch('00aa13');
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Colours Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        // Apply colors from gojekSwatch
+        primarySwatch: gojekSwatch,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Colours.swatch Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+              style: TextStyle(
+                color: gojekSwatch[700], // use color from gojekSwatch
+              ),
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: gojekSwatch[900], // use color from gojekSwatch
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
